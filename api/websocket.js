ce80fe004d99e1e50e941d89d46f0cf4b3b2da14
zickz4g <lucianorosende@gmail.com>
2022-10-18 11:42:25 -0300

    front end connection + db elimination

--- api/websocket.js
+++ api/websocket.js
@@ -10,23 +10,31 @@ const Container = new Contenedor();
 const Connect = () => {
     io.on("connection", async (socket) => {
         let dataDB = await knexstart("messages").select("*");
-        let max = 20;
         console.log("new connection");
         socket.emit("messages", dataDB);
 
-        // socket.emit("products", Container.getAll());
-        // socket.on("update", (data) => {
-        //     if ((data = "ok")) {
-        //         io.sockets.emit("products", Container.getAll());
-        //     }
-        // });
+        socket.emit("products", await Container.getWithKnex());
+        socket.on("update", async (data) => {
+            await Container.saveProduct(data);
+            io.sockets.emit("products", await Container.getWithKnex());
+        });
 
         socket.on("newMsg", async (data) => {
             await knexstart("messages").insert(data);
             let DBUpdate = await knexstart("messages").select("*");
             io.sockets.emit("messages", DBUpdate);
-            if (DBUpdate.length > max) {
+        });
+        socket.on("deleteProducts", async (data) => {
+            if (data === "delete") {
+                await Container.deleteAll();
+                io.sockets.emit("products", await Container.getWithKnex());
+            }
+        });
+        socket.on("deleteMessages", async (data) => {
+            if (data === "delete") {
                 await knexstart("messages").del();
+                let DBUpdate = await knexstart("messages").select("*");
+                io.sockets.emit("messages", DBUpdate);
             }
         });
     });
--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@ let socket = io.connect();
 socket.on("messages", (data) => {
     render(data);
 });
+socket.on("products", (products) => {
+    document.getElementById("datos").innerHTML = dataTable(products);
+});
 
 function render(data) {
     const html = data
@@ -29,4 +32,61 @@ function addMessage(e) {
     };
     socket.emit("newMsg", msg);
     return false;
+}
+
+const form = document.getElementById("tableForm");
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const data = {
+        nombre: form[0].value,
+        descripcion: form[1].value,
+        precio: form[2].value,
+        foto: form[3].value,
+        codigo: form[4].value,
+        stock: form[5].value,
+    };
+    form.reset();
+    socket.emit("update", data);
+});
+
+function dataTable(products) {
+    let res = "";
+    if (products.length) {
+        res += `
+        <style>
+            .table td, .table th {
+                vertical-align : middle;
+            }
+        </style>
+        <div class="table-responsive">
+            <table class="table table-dark">
+                <tr> <th>Nombre</th> <th>Descripcion</th> <th>Precio</th> <th>Foto</th> <th>Codigo</th> <th>Stock</th> <th>ID</th> </tr>
+        `;
+        res += products
+            .map(
+                (p) => `
+                <tr>
+                    <td>${p.nombre}</td>
+                    <td>${p.descripcion}</td>
+                    <td>$${p.precio}</td>
+                    <td><img width="50" src="${p.foto}" alt="not found"></td>
+                    <td>${p.codigo}</td>
+                    <td>${p.stock}</td>
+                    <td>${p.id}</td>
+                </tr>
+        `
+            )
+            .join(" ");
+        res += `
+            </table>
+        </div>`;
+    }
+    return res;
+}
+
+function deleteProducts() {
+    socket.emit("deleteProducts", "delete");
+}
+function deleteMessages() {
+    socket.emit("deleteMessages", "delete");
 }
